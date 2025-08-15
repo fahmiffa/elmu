34b1a107e80717f1fe3487481647448a733963c6
fahmi Fa <faisol.ajifa@gmail.com>
2025-08-16 00:53:15 +0700

    schedule

--- resources/views/schedule/form.blade.php
+++ resources/views/schedule/form.blade.php
@@ -107,7 +107,7 @@
                             </div>
                         </template>
                         <button type="button" @click="addPertemuan()"
-                            class="cursor-pointer text-orange-800 font-bold">
+                            class="cursor-pointer text-orange-600 font-bold">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="lucide lucide-file-plus2-icon lucide-file-plus-2">
